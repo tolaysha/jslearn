a541524b33030f728586407f79d97f8ef81945b9
Anatoly <tolaykuzay@gmail.com>
2019-01-20 19:43:27 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strinct'
+var CamelCase = true;
+{
+let CamelCase2 = false;
+}
+let love ={
+    man : "tolya",
+    girl : "yulya"
+}
+
+console.log(love.girl);
+//var Test = +prompt('1');
+
+let firsr = true,
+    second = TextTrackCueList;
+console.log(firsr && second)
